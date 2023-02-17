383c895ca0f9da88315f614dca66c690ba88888b
KilroyWasHere-cs-j <59028838+KilroyWasHere-cs-j@users.noreply.github.com>
2023-02-16 22:32:13 -0500

    Updated download link it now works

--- scripts/downloads.js
+++ scripts/downloads.js
@@ -1,7 +1,7 @@
 function DesktopDownloadClicked() {
     const responce = confirm("Are you sure you want to download ScoutEye Suite Desktop at this time?")
     if(responce){
-        window.location.replace("https://github.com/frc5687/ScoutEye/archive/refs/tags/v.1.9.5.zip");
+        window.location.replace("https://github.com/frc5687/ScoutEye/archive/refs/tags/v.2.0.zip");
     }
 }
 
