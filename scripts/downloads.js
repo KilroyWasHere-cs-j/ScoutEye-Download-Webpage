01251bb26d127ad7665851b089b999e4e39ca793
KilroyWasHere-cs-j <59028838+KilroyWasHere-cs-j@users.noreply.github.com>
2023-02-18 16:51:25 -0500

    .

--- scripts/downloads.js
+++ scripts/downloads.js
@@ -1,13 +1,14 @@
 function DesktopDownloadClicked() {
     const responce = confirm("Are you sure you want to download ScoutEye Suite Desktop at this time?")
     if(responce){
-        window.location.replace("https://github.com/frc5687/ScoutEye/archive/refs/tags/v.2.0.zip");
+        window.location.replace("https://github.com/frc5687/ScoutEye/archive/refs/heads/main.zip");
     }
 }
 
 function AndroidDownloadClicked(){
     const responce = confirm("Are you sure you want to download ScoutEye Android at this time?")
     if(responce){
-        alert("ScoutEye Android is currently still underdevelopment, thus not available for download.")
+        //https://github.com/frc5687/ScoutEye/blob/main/app-release.apk
+        window.location.replace("https://github.com/frc5687/ScoutEye/blob/main/app-release.apk");
     }
 }
